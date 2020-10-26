2be8cdd5c29291cce0318381c65055124db4ee73
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-26 15:48:47 +1100

    edit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "Ready to Read some Lines?"
+      message: "CLICK TO GET LINES"
     };
   }
 
